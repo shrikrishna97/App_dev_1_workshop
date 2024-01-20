2a07ca20a696c570a0e7440fa1ca03e3619a53ce
shrikrishna97 <shrikrishnapandey72@gmail.com>
2024-01-20 12:35:00 +0530

    workshop day-4

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,request, redirect, url_for
 # from jinja2 import template
-from model import db,User as user_model
+from model import db,User as user_model,Section
 
 app = Flask(__name__)
 app.secret_key = "workshop-secret-key"
@@ -54,14 +54,14 @@ def signup():
     elif request.method=='POST':
         email = request.form['email']
         passw = request.form['password']
-
+        username = request.form['username']
         c = user_model.query.filter_by(email=email).first()
         print("sign",c)
         if c:
             return render_template('DashBoard.html',email=email,passw=passw)
         else:
             info ="hey there"
-            user=user_model(email=email,password=passw)
+            user=user_model(email=email,password=passw,role="admin",username=username)
             db.session.add(user)
             db.session.commit()
             return render_template('login.html',info=info)        
@@ -69,7 +69,61 @@ def signup():
 @app.route('/dash', methods=["GET"])
 def dash():
     users1 = user_model.query.all()
-    return render_template('DashBoard.html',users=users1)       
+    data = Section.query.all()
+    return render_template('DashBoard.html',users=users1, data=data)
+
+@app.route('/upload/section/<int:user_id>', methods=['GET','POST'])
+def upload_section(user_id):
+    if request.method == 'GET':
+        return render_template('upload_section.html',id=user_id)
+    if request.method == 'POST':
+        name = request.form['sec_name']
+        desc = request.form['sec_description']
+        new_section = Section( sec_name=name, sec_description=desc,user_id=user_id)
+
+        db.session.add(new_section)
+        db.session.commit()
+        return redirect(url_for('dash'))
+
+
+    # return render_template('upload_section.html',id=id)
+
+@app.route('/update/section/<int:sec_id>', methods=['GET','POST'])
+def update_section(sec_id):
+    if request.method == 'GET':
+        return render_template('update_section.html',id=sec_id)
+    if request.method == 'POST':
+        name = request.form['sec_name']
+        Section.query.filter_by(id=sec_id).update({Section.sec_name: name})
+
+        # print(db.session.update(Section,sec_id))
+        # print(Section.query.filter_by(id=sec_id))
+        # new_section = Section( sec_name=name)
+
+        # db.session.add(new_section)
+        db.session.commit()
+        return redirect(url_for('dash'))
+    return render_template('update_section.html',id=sec_id)
+
+@app.route('/delete/section/<int:sec_id>', methods=['GET','POST'])
+def delete_section(sec_id):
+    section = Section.query.get_or_404(sec_id)
+        # Section.query.filter_by(id=sec_id).delete({Section.sec_name: name})
+    db.session.delete(section)
+        # db.session.add(new_section)
+    db.session.commit()
+    return redirect(url_for('dash'))
+    # return render_template('DashBoard.html')
+
+# @app.route('/search/<string:name>', methods=["GET"])  
+# def search(name):
+#     data = Section.query.all()
+#     if name1 in data:
+#         name1 = name
+#         return render_template('search.html')
+#     else:
+#         return render_template('error.html')
+#     # return render_template('search.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- model.py
+++ model.py
@@ -1,11 +1,29 @@
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    user_id = db.Column(db.Integer,primary_key=True,autoincrement=True)
-    email = db.Column(db.String(200),nullable=False)
-    password = db.Column(db.String(20),nullable=False)
+# class User(db.Model):
+#     user_id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+#     email = db.Column(db.String(200),nullable=False)
+#     password = db.Column(db.String(20),nullable=False)
 
-    def __repr__(self):
-        return '<User (email = %s, password = %s)>' %(self.email, self.password)
+#     def __repr__(self):
+#         return '<User (email = %s, password = %s)>' %(self.email, self.password)
+    
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)  
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    role = db.Column(db.String(20), nullable=False)
+    # ebooks = db.relationship('EBook', backref='owner', lazy=True)
+    sections = db.relationship('Section', backref='owner', lazy=True)
+
+class Section(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sec_name = db.Column(db.String(100), nullable=False)
+    sec_description = db.Column(db.Text)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+ 
